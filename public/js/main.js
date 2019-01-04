cac0df52b7a4da03ae09f8bac4ec87afff7f3c50
supersonicgabs <gabrielfonseca.coutinho@outlook.com>
2019-01-04 03:50:06 -0200

    Keyboard Input (pt2)

--- public/js/main.js
+++ public/js/main.js
@@ -7,11 +7,6 @@ import {createBackgroundLayer, createSpriteLayer} from "./layers.js";
 
 import Keyboard from "./keyboardstate.js";
 
-const input = new Keyboard();
-input.addMapping(32, keyState => {
-    console.log(keyState);
-});
-input.listenTo(window);
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
@@ -29,7 +24,19 @@ Promise.all([
 
     const gravity = 2000;
     mario.pos.set(64, 180);
-    mario.vel.set(200, -600);    
+
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+        if(keyState){
+            mario.jump.start();
+        } else{
+            mario.jump.cancel();
+        }
+        console.log(keyState);
+    });
+    input.listenTo(window);
+    
 
     const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
